29a2165c062783d4823703e2fb23b8449bb3e694
Saniya Savla <azureuser@hackutd.o2kclpowk40uvksu0n1zw5dzwg.bx.internal.cloudapp.net>
2024-11-17 05:54:27 +0000

    added cloudfare deployment to our domain at gitfile.tech

---
+++ backend/cloudfare-deployment/cf.bash
@@ -0,0 +1,11 @@
+CF_API_TOKEN="vONYIPvVmX2c6yV0OaKkCwIlCz2XtONIQgNJkznY"
+CF_EMAIL="royceaden@gmail.com"
+DOMAIN="gitlife.tech"
+
+c3 api instance create \
+  --api-token $CF_API_TOKEN \
+  --email $CF_EMAIL \
+  --name $DOMAIN \
+  --zone $DOMAIN \
+  --plan "free" \
+  --type "full"
